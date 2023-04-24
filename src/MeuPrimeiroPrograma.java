7984221b445c61742a3d0a9d869f145310c1e0b7
davilos <daviloscostagg@hotmail.com>
2023-04-24 19:08:43 -0300

    adds explanations about functions/methods

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,20 +1,35 @@
+import java.util.Scanner;
+
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        // Dois tipos de erros
+        // calc(10, 15); // Argumentos -> 10 e 5
 
-        // ERRO DE COMPILAÇÃO
+        Scanner input = new Scanner(System.in);
 
-        // .java -> .class
-        double number1 = 10.45;
-        System.out.println(number1);
+        System.out.print("Diga seu nome: ");
+        String nome = input.next();
 
+        diz_ola(nome);
 
-        // ERRO DE EXECUÇÃO
-        int x = 10;
-        int y = 0;
-
-        System.out.println(x / y);
+        input.close();
     }
 
+    static void calc(int a, int b) {
+        // static void -> Declaração padrão.
+        // calc -> Nome da função.
+        // () -> Usamos para definir o que chegará naquela função (parâmetros).
+        // (tipo nomeDaVariavel)
+        // {} -> Bloco de código que será executado.
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+    }
+
+    static void diz_ola(String nome) {
+        // System.out.printf("Olá %s", nome);
+        System.out.println("Olá " + nome);
+    }
 }
