2f21e27c0d037ac3b4a71ffc535a4e032f9a1db0
davilos <daviloscostagg@hotmail.com>
2023-04-24 17:45:11 -0300

    adds explanations about the two errors in Java

---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,20 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        // Dois tipos de erros
+
+        // ERRO DE COMPILAÇÃO
+
+        // .java -> .class
+        double number1 = 10.45;
+        System.out.println(number1);
+
+
+        // ERRO DE EXECUÇÃO
+        int x = 10;
+        int y = 0;
+
+        System.out.println(x / y);
+    }
+
+}
