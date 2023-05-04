637601f7f3b661114130177fa8551ad5ba6a6d2f
davilos <daviloscostagg@hotmail.com>
2023-05-04 17:37:14 -0300

    adds explanations about primitive types and non-primitive types

---
+++ parte-2/src/MegaSena.java
@@ -0,0 +1,36 @@
+/*
+ import co.tiagoaguiar.megasena.User;
+ import java.util.Random;
+*/
+
+public class MegaSena {
+
+    public static void main(String[] args) {
+        // Random generate = new Random();
+
+        int x = 2; // Tipo primitivo -> Servem para operações básicas.
+
+        Integer y = 4; // Tipo NÃO primitivo -> Servem para operações complexas, quando por exemplo precisamos
+        // converter o número para String ou outros tipos de dados.
+
+        String z = y.toString();
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        /*int i = 0;
+
+        while (i < 6) {
+            int number = generate.nextInt(60);
+            System.out.println(number);
+            i++;
+        }*/
+
+        /*for (int i = 0; i < 6; i++) {
+            int number = generate.nextInt(60);
+            System.out.println(number);
+        }*/
+    }
+
+}
