f5058fcd41c399c57c2353b2a4c6182ff240f806
davilos <daviloscostagg@hotmail.com>
2023-05-08 13:50:36 -0300

    deletes old directories

--- exercicios/src/Exercicio01.java
+++ exercicios/src/Exercicio01.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Teste {
+public class Exercicio01 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
