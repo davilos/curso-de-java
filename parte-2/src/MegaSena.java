b4e0312bab498e84d9abb483fc2e33e950c9d1a7
davilos <daviloscostagg@hotmail.com>
2023-05-08 13:53:49 -0300

    adds the file to the module

---
+++ parte-2/src/MegaSena.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class MegaSena {
+    public MegaSena() {
+    }
+
+    public static void main(String[] var0) {
+        byte var1 = 2;
+        Integer var2 = 4;
+        String var3 = var2.toString();
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+    }
+}
