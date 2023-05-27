5b9fc8eaa1df63d47e43288642a2a891e9ec1caf
davilos <daviloscostagg@hotmail.com>
2023-05-27 12:20:59 -0300

    adds setter and getters to the Pessoa class

--- exemplos/src/Pessoa.java
+++ exemplos/src/Pessoa.java
@@ -1,16 +1,42 @@
 public class Pessoa {
 
     // Atributos
-    public float peso;
-    public float altura;
+    private float peso;
+    private float altura;
 
     // Construtor
     Pessoa() {
 
     }
 
-    // Método
+    // Método para calcular
     public float calcularIMC() {
         return peso / (altura * altura);
     }
+
+    // Métodos acessores
+    public void setPeso(float peso) {
+        if (peso <= 0) {
+            System.out.println("Peso inválido!");
+            System.exit(0);
+        }
+        this.peso = peso;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setAltura(float altura) {
+        if (altura <= 0) {
+            System.out.println("Altura inválida!");
+            System.exit(0);
+        }
+        this.altura = altura;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
 }
