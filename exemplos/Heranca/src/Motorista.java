e47479cd1ed56941c0ac5a2bb33b18aac1ffc2f9
davilos <daviloscostagg@hotmail.com>
2023-06-08 15:27:42 -0300

    adds setters and getters to the Motorista class

--- exemplos/Heranca/src/Motorista.java
+++ exemplos/Heranca/src/Motorista.java
@@ -1,5 +1,3 @@
-import java.util.Date;
-
 public class Motorista extends Funcionario {
 
     private String nomeCarro;
@@ -7,5 +5,21 @@ public class Motorista extends Funcionario {
 
     public Motorista() {
         super();
+    }
+
+    public String getNomeCarro() {
+        return nomeCarro;
+    }
+
+    public void setNomeCarro(String nomeCarro) {
+        this.nomeCarro = nomeCarro;
+    }
+
+    public int getTotalViagensFeitas() {
+        return totalViagensFeitas;
+    }
+
+    public void setTotalViagensFeitas(int totalViagensFeitas) {
+        this.totalViagensFeitas = totalViagensFeitas;
     }
 }
