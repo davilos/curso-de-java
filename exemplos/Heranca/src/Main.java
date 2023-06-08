40f9746c0912db755f7ef0a0850966a8a2c3232e
davilos <daviloscostagg@hotmail.com>
2023-06-08 15:30:01 -0300

    adds the criarVendedor method and changes the method main

--- exemplos/Heranca/src/Main.java
+++ exemplos/Heranca/src/Main.java
@@ -1,7 +1,38 @@
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Vendedor v = new Vendedor();
+    public static void main(String[] args) throws ParseException {
+        Main mainInstance = new Main();
+        mainInstance.criarVendedor();
 
         Motorista m = new Motorista();
     }
+
+    public void criarVendedor() throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        Vendedor v = new Vendedor();
+
+        System.out.print("Digite o nome do vendedor: ");
+        v.setNome(scanner.nextLine());
+
+        System.out.print("Digite o salário do vendedor: ");
+        v.setSalario(scanner.nextFloat());
+
+        System.out.print("Digite o cpf do vendedor: ");
+        v.setCpf(scanner.next());
+
+        System.out.print("Digite a data de nascimento do vendedor: ");
+        v.setDataNascimento(scanner.next());
+
+        System.out.print("Digite a comissão do vendedor: ");
+        v.setComissaoPorItem(scanner.nextFloat());
+
+        System.out.print("Digite o total de itens vendidos do vendedor: ");
+        v.setTotalItensVendidos(scanner.nextInt());
+
+        System.out.println("O salário do vendedor é " + v.calcularSalario() + "data: " + v.getDataNascimento());
+
+    }
+
 }
